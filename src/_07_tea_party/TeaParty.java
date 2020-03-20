c34f782f1928b504499afbd7be2674b556922622
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-03-20 16:55:37 -0700

    Tea Party

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -4,10 +4,32 @@ public class TeaParty {
 	private String name;
 	private boolean isWoman;
 	private boolean isKnighted;
+	private String isWoman1;
+	private String isKnighted1;
+	TeaParty() {
+	}
 	  public String welcome(String name, boolean isWoman, boolean isKnighted) {
-	        this.name = name;
-	        this.isWoman = isWoman;
-	        this.isKnighted = isKnighted;
+		  	this.name = name;
+			this.isWoman = isWoman;
+			this.isKnighted = isKnighted;
+			if(isWoman==true) {
+				isWoman1 = "Ms.";
+			}
+			else if (isWoman == false){
+				isWoman1 = "Mr.";
+			}
+			if (isKnighted == true) {
+				isWoman1 = "Sir";
+			}
+			else if (isKnighted == false) {
+				isKnighted1 = "";
+			}
+			
+			return "Hello " + isWoman1 + " " + isKnighted1 + name;
+			//"Hello Ms. _ Austen
       }
 
+	 
 }
+	  
+	
